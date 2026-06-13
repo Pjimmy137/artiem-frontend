f963aa55f07c1953cd6d0217eea6556e4284a9ed
Pedro Martin <pedromartintorrijo@gmail.com>
2026-06-13 17:54:46 +0200

    Corregidos límites de CSS y desactivado prerender estático para rutas dinámicas

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'hoteles/:id',
+    renderMode: RenderMode.Client // 🌟 Esto le dice a Angular: "No busques parámetros al compilar, cárgalo en el cliente".
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
